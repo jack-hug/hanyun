45dcd206f34846d5a6d95587a69a0c56e65e3e6f
jack-hug <46361381@qq.com>
2024-08-16 17:25:51 +0800

    2024-08-16 photo light-move effect

--- gunicorn_start.sh
+++ gunicorn_start.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-NAME="hanyun"
+NAME="app"  # 根据你的 Flask 应用模块名称调整
 APP_DIR=/home/hanyun
 VENV_DIR=/home/hanyun/venv
 SOCKFILE=/home/hanyun/gunicorn.sock
 NUM_WORKERS=3
+
 echo "Starting $NAME"
 cd $APP_DIR
 source $VENV_DIR/bin/activate
 export PYTHONPATH=$APP_DIR:$PYTHONPATH
-exec gunicorn ${NAME}:app -b 0.0.0.0:8000 --workers=$NUM_WORKERS --bind=unix:$SOCKFILE
+
+exec gunicorn ${NAME}:app --workers=$NUM_WORKERS --bind=unix:$SOCKFILE
